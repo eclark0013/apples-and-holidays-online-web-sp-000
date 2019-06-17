bd00082dd1d9be00cdeff0ccd19e6e22f8fbc201
Eric Clark <eclark0013@gmail.com>
2019-06-17 20:10:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     specific_holiday_hash.each do |holiday_name, supply_array|
       if holiday_name.includes "_"
-        puts "  #{holiday_name.scan(/[a-z]+/).capitalize}: #{supply_array.join(", ")}"
+        puts "  #{holiday_name.scan(/[a-z]+/)}: #{supply_array.join(", ")}"
 
       else
         puts "  #{holiday_name.capitalize}: #{supply_array.join(", ")}"
